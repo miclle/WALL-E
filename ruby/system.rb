895136a969b146463d14d607f8ef1314a564d548
miclle <miclle.zheng@gmail.com>
2013-04-09 23:47:56 +0800

    Add get CPU Info method

--- ruby/system.rb
+++ ruby/system.rb
@@ -1,7 +1,46 @@
 class System
 
   def self.cpu
-    "CPU Infofasdfa dsfasdfa fasdfa"
+    cpuinfo = Hash.new
+    real_cpu = Hash.new
+    cpu_number = 0
+    current_cpu = nil
+
+    File.open("/proc/cpuinfo").each do |line|
+      case line
+      when /processor\s+:\s(.+)/
+        cpuinfo[$1] = Hash.new
+        current_cpu = $1
+        cpu_number += 1
+      when /vendor_id\s+:\s(.+)/
+        cpuinfo[current_cpu]["vendor_id"] = $1
+      when /cpu family\s+:\s(.+)/
+        cpuinfo[current_cpu]["family"] = $1
+      when /model\s+:\s(.+)/
+        cpuinfo[current_cpu]["model"] = $1
+      when /stepping\s+:\s(.+)/
+        cpuinfo[current_cpu]["stepping"] = $1
+      when /physical id\s+:\s(.+)/
+        cpuinfo[current_cpu]["physical_id"] = $1
+        real_cpu[$1] = true
+      when /core id\s+:\s(.+)/
+        cpuinfo[current_cpu]["core_id"] = $1
+      when /cpu cores\s+:\s(.+)/
+        cpuinfo[current_cpu]["cores"] = $1
+      when /model name\s+:\s(.+)/
+        cpuinfo[current_cpu]["model_name"] = $1
+      when /cpu MHz\s+:\s(.+)/
+        cpuinfo[current_cpu]["mhz"] = $1
+      when /cache size\s+:\s(.+)/
+        cpuinfo[current_cpu]["cache_size"] = $1
+      when /flags\s+:\s(.+)/
+        cpuinfo[current_cpu]["flags"] = $1.split(' ')
+      end
+    end
+
+    cpuinfo[:total] = cpu_number
+    cpuinfo[:real] = real_cpu.keys.length
+    cpuinfo
   end
 
 end
