9428c86c3f360879afc4f45d91cc2e298a858124
miclle <miclle.zheng@gmail.com>
2013-06-18 01:16:41 +0800

    Fix GoogleSpeech.sh

--- ruby/lib/GoogleSpeech.sh
+++ ruby/lib/GoogleSpeech.sh
@@ -11,23 +11,24 @@ echo "Speech Script by Dan Fountain"
 echo "TalkToDanF@gmail.com"
 echo "---------------------------"
 for key in "${!ary[@]}"
-do
-SHORTTMP[$STRINGNUM]="${SHORTTMP[$STRINGNUM]} ${ary[$key]}"
-LENGTH=$(echo ${#SHORTTMP[$STRINGNUM]})
-#echo "word:$key, ${ary[$key]}"
-#echo "adding to: $STRINGNUM"
-if [[ "$LENGTH" -lt "100" ]]; then
-#echo starting new line
-SHORT[$STRINGNUM]=${SHORTTMP[$STRINGNUM]}
-else
-STRINGNUM=$(($STRINGNUM+1))
-SHORTTMP[$STRINGNUM]="${ary[$key]}"
-SHORT[$STRINGNUM]="${ary[$key]}"
-fi
-done
+  do
+    SHORTTMP[$STRINGNUM]="${SHORTTMP[$STRINGNUM]} ${ary[$key]}"
+    LENGTH=$(echo ${#SHORTTMP[$STRINGNUM]})
+    #echo "word:$key, ${ary[$key]}"
+    #echo "adding to: $STRINGNUM"
+    if [[ "$LENGTH" -lt "100" ]]; then
+      #echo starting new line
+      SHORT[$STRINGNUM]=${SHORTTMP[$STRINGNUM]}
+    else
+      STRINGNUM=$(($STRINGNUM+1))
+      SHORTTMP[$STRINGNUM]="${ary[$key]}"
+      SHORT[$STRINGNUM]="${ary[$key]}"
+    fi
+  done
+
 for key in "${!SHORT[@]}"
-do
-#echo "line: $key is: ${SHORT[$key]}"
-echo "Playing line: $(($key+1)) of $(($STRINGNUM+1))"
-mpg123 -q "http://translate.google.com/translate_tts?tl=en&q=${SHORT[$key]}"
-done
+  do
+    #echo "line: $key is: ${SHORT[$key]}"
+    echo "Playing line: $(($key+1)) of $(($STRINGNUM+1))"
+    mpg123 -q "http://translate.google.com/translate_tts?tl=en&q=${SHORT[$key]}"
+  done
